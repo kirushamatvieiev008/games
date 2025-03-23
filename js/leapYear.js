d7ee1e828419933a5b5f49fb1d41161ff06acee2
Кірюша <mkiril087@gmail.com>
2025-03-23 16:15:53 +0200

    gh

--- js/leapYear.js
+++ js/leapYear.js
@@ -1,31 +1,33 @@
 const leapForm = document.querySelector(`.form_vysokosnyi`);
 const leaResult = document.querySelector(`.you_was_born`);
 const inpLeap = document.querySelector(`.inp_one_in_4`);
-let x = 111
+// let x = 111;
 
-inpLeap.textContent = x;
 
 
 
 leapForm.addEventListener(`submit`, (e) => {
-    e.preventDefault()
-    let inputValue = e.target.elements.leap.value
-    console.log(inputValue);
+    e.preventDefault();
+    let inputValue = e.target.elements.leap.value;
+    // console.log(inputValue);
     
     if (inputValue % 4 === 0 && (inputValue % 100 !== 0 || inputValue % 400 === 0)) {
         // leaResult.textContent(`Ви народилися у високосний рік!`)
         leaResult.textContent = `Ви народилися у високосний рік!`;
         leaResult.style.color = `#039900`;
-        // inputValue = ``;
+
         console.log(inpLeap);
 
         
     } else {
-        // console.log(`you wasn't born in leapYear`);
+
         leaResult.textContent = `Ви не народилися у високосний рік!`;
         leaResult.style.color = `red`;
-        // inputValue = ``;
+
     }
+    
+    e.target.elements.leap.value = ``;
+
     
 })
 
---
+++ js/modal2.js
@@ -0,0 +1,34 @@
+const close = document.querySelector(`.close`);
+const backdropElement = document.querySelector(`.backdrop`);
+const footerForm = document.querySelector(`.footerForm`);
+const errorFooter = document.querySelector(`.errorFooter`);
+// backdropElement.style.display = `none`;
+
+
+footerForm.addEventListener(`submit`, (e) => {
+    e.preventDefault();
+    let inpFooter = e.target.elements.inpFooter.value;
+    console.log(inpFooter);
+    
+    if (inpFooter.includes(`@`) && inpFooter.includes(`.com`) && inpFooter.length > 5) {
+        // backdropElement.style.display = `block`;
+        backdropElement.classList.remove(`hidden`)
+        errorFooter.textContent = ``;
+        // inpFooter.textContent() = ``;
+        
+
+    } else {
+        errorFooter.textContent = `Введіть усе правильно!`;
+    }
+        
+
+})
+
+close.addEventListener(`click`, (e) => {
+
+    
+    backdropElement.classList.add(`hidden`)
+    // console.log(e.target);
+    // console.log(e.currentTarget);
+    
+})
