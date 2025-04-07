c78d653385c1b672102b2e5525eba4f835e7fd7e
Кірюша <mkiril087@gmail.com>
2025-04-07 17:03:04 +0300

    пр

--- js/game4.js
+++ js/game4.js
@@ -7,7 +7,22 @@ const resultDiv = document.querySelector(`.result`);
 
 const calcButtons = document.querySelectorAll(`.calc`);
 
-form1.addEventListener(`submit`, (e) => {
+
+
+// form1.addEventListener(`change`, (e) => {
+//     e.preventDefault();
+//     let valueFirst = 0;
+//     let valueSecond = 0;
+//     if (e.target.classList.contains(`num1`)) {
+//         valueFirst = Number(e.target.value);
+//     } 
+
+//     if (e.target.classList.contains(`num2`)) {
+//         valueSecond = Number(e.target.value);
+//         valueFirst = valueFirst;
+//     }
+
+    form1.addEventListener(`submit`, (e) => {
     e.preventDefault();
     const valueFirst = e.currentTarget.elements.value1.value;
     // console.log(valueFirst);
@@ -34,3 +49,34 @@ form1.addEventListener(`submit`, (e) => {
     
 
 })
+
+
+//     calcButtons.forEach((btn) => {
+    
+    
+//         let result = 0;
+//         // let resultDiv;
+        
+//         btn.addEventListener(`click`, (e) => {
+//             if (e.target.classList.contains(`plus`)) {
+//             result = valueFirst + valueSecond;
+//             // console.log(result);
+//             console.log(valueFirst, valueSecond);
+            
+//         } else if (e.target.classList.contains(`mnozhenya`)) {
+//             result = valueFirst * valueSecond;   
+//         } else if (e.target.classList.contains(`minus`)) {
+//             result = valueFirst - valueSecond;   
+//         } else {
+//             result = valueFirst / valueSecond;   
+//         } 
+        
+//         resultBtn.addEventListener(`click`, (e) => {
+//             resultDiv.textContent = result;
+            
+//         })
+        
+//     })
+// });
+    
+// })
