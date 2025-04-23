1e4ebb63864cd5bff7985980eede2fe846e3a890
Кірюша <mkiril087@gmail.com>
2025-04-23 21:44:57 +0300

    gh

--- js/slider.js
+++ js/slider.js
@@ -5,9 +5,13 @@ const student3 = document.querySelector(`.inf3`);
 const arrayStudents = document.querySelectorAll(`.student`);
 const buttonSlideLeft = document.querySelector(`.slide_button_left`);
 const buttonSlideRight = document.querySelector(`.slide_button_right`);
+const line1 = document.querySelector(`.line1`);
+const line2 = document.querySelector(`.line2`);
+const line3 = document.querySelector(`.line3`);
 
 let arrayClics = [];
 let clics = 0;
+let clics2 = 0;
 
 buttonSlideRight.addEventListener(`click`, (e) => {
     clics++;
@@ -24,15 +28,92 @@ buttonSlideRight.addEventListener(`click`, (e) => {
         student2.style.display = `none`;
         student3.style.display = `none`;
 
+        line1.style.width = `30px`;
+        line2.style.width = `18px`;
+        line3.style.width = `18px`;
+
+        line1.style.backgroundColor = `#797979`;
+        line2.style.backgroundColor = `#D9D9D9`;
+        line3.style.backgroundColor = `#D9D9D9`;
+
     } else if (arrayStudents[clics].classList.contains(`inf2`)) {
         student1.style.display = `none`;
         student2.style.display = `flex`;
         student3.style.display = `none`;
+
+        line1.style.width = `18px`;
+        line2.style.width = `30px`;
+        line3.style.width = `18px`;
+
+        line1.style.backgroundColor = `#D9D9D9`;
+        line2.style.backgroundColor = `#797979`;
+        line3.style.backgroundColor = `#D9D9D9`;
+
     } else {
         student1.style.display = `none`;
         student2.style.display = `none`;
         student3.style.display = `flex`;
+
+        line1.style.width = `18px`;
+        line2.style.width = `18px`;
+        line3.style.width = `30px`;
+
+        line1.style.backgroundColor = `#D9D9D9`;
+        line2.style.backgroundColor = `#D9D9D9`;
+        line3.style.backgroundColor = `#797979`;
     }
     
 })
 
+
+
+buttonSlideLeft.addEventListener(`click`, (e) => {
+    clics2++;
+    if (clics2 > 2) {
+        clics2 = 0;
+    } else {
+
+    }
+    // arrayStudents[clics].style.display = `block`;
+    // arrayStudents[clics - 1].style.display = `none`;
+
+    if (arrayStudents[clics2].classList.contains(`inf1`)) {
+        student1.style.display = `none`;
+        student2.style.display = `none`;
+        student3.style.display = `flex`;
+
+        line1.style.width = `18px`;
+        line2.style.width = `18px`;
+        line3.style.width = `30px`;
+
+        line1.style.backgroundColor = `#D9D9D9`;
+        line2.style.backgroundColor = `#D9D9D9`;
+        line3.style.backgroundColor = `#797979`;
+
+    } else if (arrayStudents[clics2].classList.contains(`inf2`)) {
+        student1.style.display = `none`;
+        student2.style.display = `flex`;
+        student3.style.display = `none`;
+
+        line1.style.width = `18px`;
+        line2.style.width = `30px`;
+        line3.style.width = `18px`;
+
+        line1.style.backgroundColor = `#D9D9D9`;
+        line2.style.backgroundColor = `#797979`;
+        line3.style.backgroundColor = `#D9D9D9`;
+    } else {
+        student1.style.display = `flex`;
+        student2.style.display = `none`;
+        student3.style.display = `none`;
+
+        line1.style.width = `30px`;
+        line2.style.width = `18px`;
+        line3.style.width = `18px`;
+
+        line1.style.backgroundColor = `#797979`;
+        line2.style.backgroundColor = `#D9D9D9`;
+        line3.style.backgroundColor = `#D9D9D9`;
+    }
+    
+})
